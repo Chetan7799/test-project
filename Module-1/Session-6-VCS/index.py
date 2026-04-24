482c1c2fed28f0826ebbe38ee4f0e5ba5c07221e
Chetan Kumarkar <chetankumarkar7@gmail.com>
2026-04-24 10:23:49 +0530

    Added Module-1/Session-6-VCS with index.py for even numbers

---
+++ Module-1/Session-6-VCS/index.py
@@ -0,0 +1,9 @@
+# Program to print all even numbers from a given list
+
+def print_even_numbers(numbers):
+    evens = [num for num in numbers if num % 2 == 0]
+    print("Even numbers:", evens)
+
+# Example usage
+sample_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print_even_numbers(sample_list)
